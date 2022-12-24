37fbfffeb35fd407b9bdae6952f76b87138a714c
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-12-24 06:46:13 -0600

    connect the ui to the model

--- app/src/main/java/com/dk/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/dk/quizapp/QuizQuestionsActivity.kt
@@ -3,13 +3,58 @@ package com.dk.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    //Create global variables for the views in the layout
+    private var progressBar: ProgressBar?=null
+    private var tvProgress: TextView? = null
+    private var tvQuestion:TextView? = null
+    private var ivImage: ImageView? = null
+    private var tvOptionOne:TextView? = null
+    private var tvOptionTwo:TextView? = null
+    private var tvOptionThree:TextView? = null
+    private var tvOptionFour:TextView? = null
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
         super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
         setContentView(R.layout.activity_quiz_questions)
 
         val questionsList = Constants.getQuestions()
-        Log.i("Questions list size is", "${questionsList.size}")
+        Log.e("Questions Size", "${questionsList.size}")
+        for (i in questionsList) {
+            Log.e("Questions", i.question)
+        }
+        val currentPosition = 1 // Default and the first question position
+        val question: Question? =
+            questionsList[currentPosition - 1] // Getting the question from the list with the help of current position.
+
+        progressBar=findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+        progressBar?.progress =
+            currentPosition // Setting the current progress in the progressbar using the position of question
+        tvProgress?.text =
+            "$currentPosition" + "/" + progressBar?.getMax() // Setting up the progress text
+
+        // Now set the current question and the options in the UI
+        tvQuestion?.text = question!!.question
+        ivImage?.setImageResource(question.image)
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
     }
 }
